497c2368f158f8976bcd1cb977d0154eb7addf3e
Jörg Richter <jri@deepamehta.de>
2010-09-19 15:45:25 +0200

    Adapted to current version of deepamehta3-client.

--- src/main/resources/web/script/icon_field_renderer.js
+++ src/main/resources/web/script/icon_field_renderer.js
@@ -1,6 +1,6 @@
 function IconFieldRenderer(doc, field, rel_topics) {
 
-    var plugin = get_plugin("dm3_iconpicker")
+    var plugin = dm3c.get_plugin("dm3_iconpicker")
 
 
 
@@ -12,7 +12,7 @@ function IconFieldRenderer(doc, field, rel_topics) {
 
     this.render_field = function() {
         // field label
-        render.field_label(field)
+        dm3c.render.field_label(field)
         // field value
         return render_topics_icon(rel_topics)
     }
@@ -25,18 +25,18 @@ function IconFieldRenderer(doc, field, rel_topics) {
     }
 
     this.read_form_value = function() {
-        var icons = get_doctype_impl(doc).topic_buffer[field.uri]
+        var icons = dm3c.get_doctype_impl(doc).topic_buffer[field.uri]
         var old_icon_id = icons.length && icons[0].id
         var new_icon_id = $("[field-uri=" + field.uri + "] img").attr("icon-topic-id")
         if (old_icon_id) {
             if (old_icon_id != new_icon_id) {
                 // re-assign icon
-                delete_relation(dmc.get_relation(doc.id, old_icon_id).id)
-                create_relation("RELATION", doc.id, new_icon_id)
+                dm3c.delete_relation(dm3c.restc.get_relation(doc.id, old_icon_id).id)
+                dm3c.create_relation("RELATION", doc.id, new_icon_id)
             }
         } else if (new_icon_id) {
             // assign icon
-            create_relation("RELATION", doc.id, new_icon_id)
+            dm3c.create_relation("RELATION", doc.id, new_icon_id)
         }
         // prevent this field from being updated
         return null
